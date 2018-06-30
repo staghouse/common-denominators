59c900eb8e0b3d57d17a1ebba43c1cde84d322ae
Eric Lakatos <1939508+ericlakatos@users.noreply.github.com>
2018-06-30 01:16:52 -0400

    Elakatos (#4)
    * Update: Redefined benchmarks for additional testing.

--- test/cases/arrays.js
+++
@@ -1,7 +0,0 @@
-let testCaseArrays = [];
-
-for (let i = 0; i < 10000; i++) {
-    testCaseArrays.push(Math.floor(Math.random() * 10000));
-}
-
-module.exports = testCaseArrays;
--- test/index.stable.benchmark.js
+++ test/index.stable.benchmark.js
@@ -1,12 +1,16 @@
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite();
 
-const testCase = require('./cases/arrays');
+const testCaseFew = require('./cases/few');
+const testCaseMany = require('./cases/many');
 const commonDenominators = require('../build/index.min');
 
 suite
-    .add('Stable Release', function() {
-        commonDenominators(...testCase);
+    .add('Stable Release: Few', function() {
+        commonDenominators(...testCaseFew);
+    })
+    .add('Stable Release: Many', function() {
+        commonDenominators(...testCaseMany);
     })
     .on('cycle', function(event) {
         console.info('\x1b[33m%s\x1b[0m', String(event.target));
