42caf7bd37030972d83b45c6a66be731735a4b5b
Emett Speer <espeer@fortinet.com>
2018-07-03 11:41:46 -0700

    Updated build

--- build/index.js
+++ build/index.js
@@ -1,22 +1,16 @@
-function commonDenominators() {
-    let commonDenominators = [];
-    const allNumerators = Array.from(new Set(Object.values(arguments).filter(numerator => Number.isInteger(numerator) && numerator > 0))).sort((denominator, next) => denominator - next);
-    const allDenominators = allNumerators.reduce((denominators, numerator, index, numerators) => {
+const commonDenominators = (...args) => {
+    const allNumerators = Array.from(new Set(args.filter(numerator => Number.isInteger(numerator) && numerator > 0))).sort((denominator, next) => denominator - next);
+    const numeratorsLength = allNumerators.length - 1;
+    return allNumerators.reduce((denominators, numerator, index, numerators) => {
         let counter = numerators[0];
         while (counter > 0) {
             numerator % counter === 0 && numerators[0] % counter === 0 ? denominators.push(counter) : null;
             counter--;
         }
         return denominators;
-    }, []).sort((denominator, next) => denominator - next);
-    Array.from(new Set(allDenominators)).reduce((counter, denominator) => {
-        allNumerators.forEach(numerator => {
-            numerator % denominator === 0 ? counter++ : false;
-        });
-        counter === allNumerators.length ? commonDenominators.push(denominator) : null;
-        return counter = 0;
-    }, 0);
-    return commonDenominators;
-}
+    }, []).sort((denominator, next) => denominator - next).filter((denominator, index, denominators) => {
+        return denominators[index] === denominators[index + numeratorsLength];
+    });
+};
 
 module.exports = commonDenominators;
